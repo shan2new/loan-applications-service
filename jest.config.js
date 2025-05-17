5ebf59b26533b0c1739c34f1dc0a99ab594bf8d9
Shantanu Sinha <shantanusinha95@gmail.com>
2025-05-17 10:40:03 +0530

    Update dependencies and configuration: add jest-junit for test reporting, update .gitignore to include coverage, and adjust jest config for ESLint compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
